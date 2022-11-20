10df041e0317cfc24619f4126584ee5aabba5d99
Marcelo <marcelo.gomj@gmail.com>
2022-11-20 19:25:01 -0300

    add: _document Next

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import Document, { Html, Main, NextScript, Head } from "next/document"
+
+export default class name extends Document {
+
+   render(){
+      return (
+         <Html>
+            <Head>
+            </Head>
+            <body>
+               <Main />
+               <NextScript />
+            </body>
+         </Html>
+      )
+   }
+}
